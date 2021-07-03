65f6e1bd2fa2b377c709bd5a755ec985949ba3e3
Leonid Polozkov <385tJDgx5#6*t>
2021-07-03 22:22:35 +0300

    added personal account controller

---
+++ Controllers/PersonalAccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project.pole.Controllers
+{
+    public class PersonalAccountController: Controller
+    {
+        private readonly ILogger<PersonalAccountController> _logger;
+
+        public PersonalAccountController(ILogger<PersonalAccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+        [Route("PersonalAccount/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
