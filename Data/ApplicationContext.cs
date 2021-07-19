3000bcf554f72c10537322c3dc01b3105327681d
Leonid Polozkov <385tJDgx5#6*t>
2021-07-20 00:39:40 +0300

    added database connection

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using project.pole.Models;
+using System.Linq;
+
+
+namespace project.pole.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options) 
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // modelBuilder.Entity<User>().HasData(new User { Login = "admin", Password = "admin" });
+        }
+
+    }
+}
