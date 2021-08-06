9c843fea2fe99ddd34410dd47522be5c4c0c6032
Leonid Polozkov <385tJDgx5#6*t>
2021-08-07 00:22:44 +0300

    added properties to object

--- Models/Object.cs
+++ Models/Object.cs
@@ -9,5 +9,25 @@ namespace project.pole.Models
         [Column("name")]
         [Display(Prompt = "Наименование организации")]
         public string Name{ get; set; }
+        
+        [Column("region")]
+        [Display(Prompt = "Область")]
+        public string Region { get; set; }
+        
+        [Column("district")]
+        [Display(Prompt = "Регион/Район")]
+        public string District{ get; set; }
+        
+        [Column("city")]
+        [Display(Prompt = "Город/Населенный пункт")]
+        public string City{ get; set; }
+        
+        [Column("street")]
+        [Display(Prompt = "Улица")]
+        public string Street{ get; set; }
+        
+        [Column("building")]
+        [Display(Prompt = "Дом")]
+        public string Building{ get; set; }
     }
 }
