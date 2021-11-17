d80b0b94ead9c1acc4458e701e899579bae73a61
Leonid <Lenya.g.d@mail.ru>
2021-11-18 00:00:49 +0300

    refactoring

--- Models/Price.cs
+++ Models/Price.cs
@@ -8,34 +8,49 @@ namespace project.pole.Models
     {
         [Display(Name = "Отбор поверхностных проб грунта")]
         SamplingSurfaceSoilSamples, 
+        
         [Display(Name = "Отбор грунта из скважин на гаммо-спектометрию")]
         SamplingSoilFromWellsGammaSpectrometry,
+        
         [Display(Name = "Отбор грунта из скважин на сонетарно-химическое обследование")]
         SoilSamplingFromWellsFsanitaryAndChemicalInspection, 
+        
         [Display(Name = "γ-съемка. Полевые работы")]
         GammasHootingFieldWork,
+        
         [Display(Name = "γ-съемка. Камеральные работы")]
         GammasHootingDeskWork,
+        
         [Display(Name = "γ-спектрометрия")]
         GammaSpectrometry,
+        
         [Display(Name = "Измерение плотности потока радона из грунта. Полевые работы")]
         MeasurementRadonFluxDensityFromGroundFieldWork,
+        
         [Display(Name = "Измерение плотности потока радона из грунта. Камеральные работы")]
         MeasurementRadonFluxDensityFromGroundDeskWork,
+        
         [Display(Name = "Измерение плотности потока радона из грунта. Бета-спектрометрия")]
         MeasurementRadonFluxDensityFromGroundBetaSpectrometry,
+        
         [Display(Name = "Пробоподготовка")]
         SamplePreparation,
+        
         [Display(Name = "Определение тяжелых металлов")]
         DeterminationHeavyMetals,
+        
         [Display(Name = "Определение нефтепродуктов")]
         DefinitionPetroleumProducts,
+        
         [Display(Name = "Определение бензапирена")]
         DefinitionBenzopyrene,
+        
         [Display(Name = "Санитарно - экологическая экспертиза инструментальных и лабораторных исследований в составе предпроектной(проектной) документации с гигиеническими рекомендациями(радиация)")]
         SanitaryEnvironmentalExpertiseRadiation,
+        
         [Display(Name = "Санитарно-экологическая экспертиза инструментальных и лабораторных исследований в составе предпроектной (проектной) документации с гигиеническими рекомендациями (химия и бактериология)")]
         SanitaryEnvironmentalExpertiseChemistryAndBacteriology,
+        
         [Display(Name = "Бактериология")]
         Bacteriology
     }
